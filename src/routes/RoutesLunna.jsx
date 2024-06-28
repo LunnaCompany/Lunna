5ad9585da0d32e3df70de6bc1c57d5a45d50217c
ANTHONY <anthonychukwudiDev@outlook.com>
2024-06-28 01:13:32 -0300

    alteraçoes finais

--- src/routes/RoutesLunna.jsx
+++ src/routes/RoutesLunna.jsx
@@ -275,6 +275,7 @@ export const RoutesLunna = () => {
                         <LumiIconComponent />
                     </TemplateChat>
                 }
+                
             />
             
         </Routes>
